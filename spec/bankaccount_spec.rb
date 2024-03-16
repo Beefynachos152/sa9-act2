a689820acaff14ec3c7588d7cf9da450ffaf2795
Christopher Landaverde <christopher4780@gmail.com>
2024-03-16 00:44:09 -0500

    Redo for SA9 Act2

--- spec/bankaccount_spec.rb
+++ spec/bankaccount_spec.rb
@@ -12,19 +12,22 @@ RSpec.describe BankAccount do
 
   describe "#withdraw" do
     it "decreases the balance by the withdrawal amount if funds are available" do
+      account = BankAccount.new(1000)
       account.withdraw(500)
       expect(account.balance).to eq(500)
     end
 
     it "does not change the balance if insufficient funds" do
+      account = BankAccount.new(1000)
       account.withdraw(1500)
-      expect(account.balance).to eq(500)
+      expect(account.balance).to eq(1000)
     end
   end
 
   describe "#balance" do
     it "returns the current balance" do
-      expect(account.balance).to eq(500)
+      account = BankAccount.new(1000)
+      expect(account.balance).to eq(1000)
     end
   end
 end
